d4c5f99199e27b3c3d3e3d4fe9c35044f456410e
favibe <ibefavour85@gmail.com>
2025-08-28 14:10:42 +0100

    Create operate.py

---
+++ operate.py
@@ -0,0 +1,10 @@
+#Aruthemetic Operator
+a = 15
+b = 4
+print("Addition:", a+b)
+print("Substraction:", a-b)
+print("Multiplication:", a*b)
+print("Division:", a/b)
+print("Floor Division:", a//b)
+print("Modulus:", a%b)
+print("Exponention:", a**b)
