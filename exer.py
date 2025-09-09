5cc9df0687f813e71dbc674ba4d2886a5f87d0ab
favibe <ibefavour85@gmail.com>
2025-09-09 10:11:09 +0100

    args and kwargs

--- exer.py
+++ exer.py
@@ -2,3 +2,43 @@
 n = 6
 for i in range(1, n+1):
     print("square of", i, "is", i**2)
+#function
+#default argumment
+def profile(name, age=23):
+    print(f"Name: {name}, Age: {age}")
+profile("John")
+profile("Mary", 25)
+# profile() #would throw an error
+#Keyword argument
+def student(fname, lname):
+    print(fname, lname)
+student(fname="Sherk", lname="Bob")
+student(lname="Bill", fname="Sherk")
+
+#Positional Argument
+#here the argument must come in order
+def nameAge(name, age):
+    print("my name is: ", name)
+    print("my age is ", age)
+print("case-1")
+nameAge("Suraj", 27)
+print("\n case-2")
+nameAge(25,"Suraj")
+#in this argument position/order matter
+#ARGS AND KWARGS
+def add(*args):
+    return sum(args)
+print(add(1,3,4))
+print(add(2,3))
+#kwargs
+def show_info(**kwargs):
+    print(kwargs)
+show_info(name="Alice", age=23, city="London")
+#INNER/NESTED FUNCTIONS
+def fun1():
+    s = "I love coding"
+    def fun2():
+        print(s)
+    fun2()
+fun1()
+#Anonymous Functions
