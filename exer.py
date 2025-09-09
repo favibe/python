1774575e6f7d9a8eafb0f1760f8a8ab37ac6de82
favibe <ibefavour85@gmail.com>
2025-09-09 16:50:55 +0100

    map, filter and reduce method

--- exer.py
+++ exer.py
@@ -74,3 +74,75 @@ def factorial(n):
         return n * factorial(n - 1)
 
 print(factorial(5))  # 120
+#more lambda functions
+li = [lambda arg = x: arg *10 for x in range(1,6)]
+for i in li:
+    print(i())
+#more example 2
+cal = lambda x, y: (x + y, x * y)
+res = cal(11, 10)
+print(res)
+#with filter(lambda)
+n = [1,2,3,4,5,6,7,8,9]
+even = filter(lambda x: x % 2 == 0, n)
+print(list(even))
+#with map
+a = [11,22,32,44]
+b = map(lambda x: x *2, a)
+print(list(b))
+#with reduce
+from functools import reduce
+c = [2,3,4,5]
+d = reduce(lambda x,y: x * y, c)
+print(d)
+#map() with mutiple iterable
+aa = [1,2,3,4]
+bb = [5,6,4,7]
+res1 = map(lambda x,y: x + y, aa, bb)
+print(list(res1))
+#more 3
+#upper() convert ti uppercase
+#strip() removes whitespace from string
+#convert to uppercase with map()
+fruit = ["apple", "banana","cherry"]
+res2 = map(str.upper, fruit)
+print(list(res2))
+res3 = map(lambda s: s[0],fruit)
+print(list(res3)) # extracting  the first charcter
+#filter
+#filtering falsy value
+A =["Apple", "","Banana", 0, "Cherry",None]
+L = filter(None, A)
+print(list(L))
+#filter words greater than 5
+aaa = ["apple", "banana", "cherry", "kiwi", "grape"]
+bbb= filter(lambda w: len(w) > 5, aaa)
+print(list(bbb))
+#using filter and map(filter even num, map double it)
+a1 = [1, 2, 3, 4, 5, 6]
+b1 = filter(lambda x: x % 2 == 0, a1)
+c1 = map(lambda x: x * 2, b1)
+print(list(c1))
+#filter example
+def starts_a(w):
+    return w.startswith("a")
+
+li1 = ["apple", "banana", "avocado", "cherry", "apricot"]
+ress = filter(starts_a, li1)
+print(list(ress))
+#reduce 
+import functools
+import operator
+list = [1, 3, 5, 6, 2]
+
+print(functools.reduce(operator.add, list))
+print(functools.reduce(operator.mul, list)) 
+print(functools.reduce(operator.add, ["geeks", "for", "geeks"]))
+#2 reduce()
+from functools import reduce
+def add(x, y):
+    return x + y
+
+a2 = [1, 2, 3, 4, 5]
+res4 = reduce(add, a2)
+print(res4)
