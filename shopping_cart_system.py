087343e8cd60b369b5f5a7c6a764ffe6eaa0853e
favibe <ibefavour85@gmail.com>
2025-09-10 11:33:01 +0100

    Create shopping_cart_system.py

---
+++ shopping_cart_system.py
@@ -0,0 +1,12 @@
+# A console based project
+#Name, price, category
+products  = [
+    ("Apple", 200, "Fruits"),
+    ("Banana", 100, "Fruit"),
+    ("Bread", 250, "Bakery"),
+    ("Milk", 500, "Dairy"),
+    ("Cheese", 800, "Dairy"),
+    ("Eggs", 600, "Protein"),
+    ("Rice", 1200, "Grain"),
+    ("Chicken", 2500, "Meat"),
+]
