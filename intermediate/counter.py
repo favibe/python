52ab782e6a4d2e4a57511e9d446cdee1db126494
favibe <ibefavour85@gmail.com>
2025-09-10 15:33:11 +0100

    Counter Collections

---
+++ intermediate/counter.py
@@ -0,0 +1,60 @@
+from collections import Counter 
+#Create a  counter from a list, string, dict, (any iterable)
+ctr1 = Counter([1,2,3,4,5,6,2,1,2,3])
+ctr2 = Counter({1:3, 2:2, 3:1})
+ctr3 = Counter("Hello")
+print(ctr1)
+print(ctr2)
+print(ctr3)
+#Acessing  Counter Elements
+print(ctr1[1]) # 2
+print(ctr1[22]) #elemnt not present > 0
+#Updating conuter
+ctr4 = Counter([1,2,2])
+#Adding new elemnt
+ctr4.update([2,3,3,3])
+print(ctr4)
+#Counter Methods
+#elements 
+"""Returns an iterator over elements, repeating each element as many times as its count.
+Order is not guaranteed (depends on insertion)"""
+ctr1 = Counter([1,2,3,4,5,6,2,1,2,3])
+items  = list(ctr1.elements())
+print(f"print the element order for {ctr1} using element method : {items}")
+#most_common()
+c = Counter("banana")
+print(c)
+# Counter({'a': 3, 'n': 2, 'b': 1})
+print(c.most_common())    # all in descending order
+# [('a', 3), ('n', 2), ('b', 1)]
+print(c.most_common(1))   # only the top 1
+# [('a', 3)]
+common = ctr1.most_common(2)
+print(f"most common in {ctr1} is {common}")
+#subtract
+"""Subtracts counts from another iterable or mapping.
+Counts can go negative."""
+c = Counter(a=4, b=2, c=0)
+c.subtract({'a': 2, 'b': 3, 'c': 1})
+print(c)
+# Counter({'a': 2, 'c': -1, 'b': -1})
+#ex2 on subtract
+ctr = Counter([1, 2, 2, 3, 3, 3])
+ctr.subtract([2, 3, 3])
+print(ctr)
+#Arithmetic Operations on Count
+"""Counters support addition, subtraction, intersection union operations, 
+allowing for various arithmetic operations."""
+ctr5 = Counter([1,2,2,3])
+ctr6 = Counter([2,3,3,4])
+print(ctr5 +ctr6)
+print(ctr5 - ctr6)
+print(ctr5 | ctr6)#union(Takes the maximum of counts for each key.) =only keys that exist in both counters are kept.
+print(ctr5 & ctr6) #Intersection(Takes the minimum of counts for each key)=Here, all keys from both counters are kept.
+"""Key Difference
+Intersection (&) → only keys in both, counts = min
+Union (|) → all keys, counts = max"""
+#OrderedDict in Python
+#from collections import OrderedDict
+from collections import OrderedDict
+
