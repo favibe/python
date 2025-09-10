9febbf5ceb09afdf3b1bd1cd196b1310f3ab7ad9
favibe <ibefavour85@gmail.com>
2025-09-10 12:03:12 +0100

    display product func

--- shopping_cart_system.py
+++ shopping_cart_system.py
@@ -9,4 +9,15 @@ products  = [
     ("Eggs", 600, "Protein"),
     ("Rice", 1200, "Grain"),
     ("Chicken", 2500, "Meat"),
-]
+] 
+cart = {}
+#function to display product
+def show_product():
+    print("\n Availiable Product")
+    for index,(name, price,category) in enumerate (products, start=1):
+        print(f"{index}-{name}- ₦{price}-({category})")
+show_product()
+
+#Add Item To Cart
+def add_item():
+    
