03241c2a55c1f446cf6b9a0fb42e6837351cf580
favibe <ibefavour85@gmail.com>
2025-09-17 11:40:38 +0100

    abstraction

--- intermediate/abs.py
+++ intermediate/abs.py
@@ -1,2 +1,22 @@
 #data Abstraction
 """Data abstraction means showing only the essential features and hiding the complex internal details."""
+"""Abstract classes are created using abc module and @abstractmethod decorator,
+ allowing developers to enforce method implementation in subclasses while hiding complex internal logic."""
+from abc import ABC, abstractmethod
+
+class greet(ABC):
+    @abstractmethod
+    def say_hello(self):
+        pass #Abstarct method
+
+class English(greet):
+    def say_hello(self):
+        return "hello"
+    
+g = English()
+print(g.say_hello())
+
+#Abstarct Method
+"""Abstract methods are method declarations without a body defined inside an abstract class. 
+They act as placeholders that force subclasses to provide their own specific implementation, 
+ensuring consistent structure across derived classes."""
